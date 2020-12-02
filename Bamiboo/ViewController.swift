04d6a73db0d0d2382feec0416b1a9d162303592c
Bamiboo <hobin1019@naver.com>
2020-12-02 10:47:00 +0900

    Initial Commit

---
+++ Bamiboo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bamiboo
+//
+//  Created by Bamiboo on 2020/12/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
