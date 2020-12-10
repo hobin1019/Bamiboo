327446e4c64d1071d50e5f511efe14f664449bb9
HOB <hobin1019@naver.com>
2020-12-10 17:21:10 +0900

    commit

--- Bamiboo/MainViewController.swift
+++ Bamiboo/MainViewController.swift
@@ -15,6 +15,7 @@ struct CellItem {
 let viewControllerList: [[CellItem]] = [
     [
         CellItem(class: SwipeViewController.self, title: "알람 페이지"),
+        CellItem(class: CollectionViewController.self, title: "콜렉션 뷰 테스트"),
     ], [
         CellItem(class: LayoutFrameMaskViewController.self, title: "Layout, Frame"),
         CellItem(class: LayoutAutoresizingMaskViewController.self, title: "Layout, Autoresizing Mask"),
