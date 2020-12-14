fb3a997cea9169a1ed07ff7d0cbeb06a38253049
Bamiboo <hobin1019@naver.com>
2020-12-14 14:38:46 +0900

    내 소식 탭 Cell UI 추가

--- Bamiboo/AlarmTest/AlarmMyNews/Views/AlarmMyNewsCell.swift
+++ Bamiboo/AlarmTest/AlarmMyNews/Views/AlarmMyNewsCell.swift
@@ -11,12 +11,18 @@ import UIKit
 class AlarmMyNewsCell: UICollectionViewCell {
     
     // MARK: Views
+    private var titleView: UIView = {
+        let v = UIView()
+        v.translatesAutoresizingMaskIntoConstraints = false
+        v.backgroundColor = .clear
+        return v
+    }()
     private var titleLabel: UILabel = {
         let l = UILabel()
         l.translatesAutoresizingMaskIntoConstraints = false
         l.textAlignment = .left
         l.textColor = .white
-        l.font.withSize(100)
+        l.font = UIFont.boldSystemFont(ofSize: 20.0)
         l.numberOfLines = 2 // 최대 2줄
         l.text = ""
         return l
@@ -25,9 +31,16 @@ class AlarmMyNewsCell: UICollectionViewCell {
         let l = UILabel()
         l.translatesAutoresizingMaskIntoConstraints = false
         l.textAlignment = .left
-        l.textColor = .lightGray
+        l.textColor = .gray
         l.text = ""
         return l
+    }()
+    private var redDotView: UIView = {
+        let v = UIView()
+        v.translatesAutoresizingMaskIntoConstraints = false
+        v.backgroundColor = .red
+        v.layer.cornerRadius = 3
+        return v
     }()
     private var imageView: UIImageView = {
         let iv = UIImageView()
@@ -49,24 +62,36 @@ class AlarmMyNewsCell: UICollectionViewCell {
     private func commonInit() {
         contentView.backgroundColor = .black
         
-        self.addSubview(titleLabel)
-        self.addSubview(timeLabel)
         self.addSubview(imageView)
+        self.addSubview(titleView)
+        titleView.addSubview(titleLabel)
+        titleView.addSubview(timeLabel)
+        titleView.addSubview(redDotView)
         
         NSLayoutConstraint.activate([
-            // titleLabel
-            titleLabel.firstBaselineAnchor.constraint(equalTo: self.topAnchor, constant: 40),
-            titleLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 5),
-            // timeLabel
-            timeLabel.firstBaselineAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 24),
-            timeLabel.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
-            timeLabel.trailingAnchor.constraint(equalTo: titleLabel.trailingAnchor),
             // imageView
             imageView.topAnchor.constraint(equalTo: self.topAnchor),
             imageView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
-            imageView.leadingAnchor.constraint(equalTo: titleLabel.trailingAnchor, constant: 5),
             imageView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
             imageView.widthAnchor.constraint(equalTo: imageView.heightAnchor),
+            // titleView
+            titleView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10),
+            titleView.trailingAnchor.constraint(equalTo: imageView.leadingAnchor, constant: -10),
+            titleView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            // titleLabel
+            titleLabel.topAnchor.constraint(equalTo: titleView.topAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: titleView.leadingAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: titleView.trailingAnchor),
+            // timeLabel
+            timeLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 10),
+            timeLabel.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
+            timeLabel.bottomAnchor.constraint(equalTo: titleView.bottomAnchor),
+            // redDotView
+            redDotView.widthAnchor.constraint(equalToConstant: 6),
+            redDotView.heightAnchor.constraint(equalTo: redDotView.widthAnchor, multiplier: 1),
+            redDotView.centerYAnchor.constraint(equalTo: timeLabel.centerYAnchor),
+            redDotView.leadingAnchor.constraint(equalTo: timeLabel.trailingAnchor, constant: 10),
+            redDotView.trailingAnchor.constraint(lessThanOrEqualTo: titleView.trailingAnchor),
         ])
     }
     
@@ -95,6 +120,17 @@ struct AlarmMyNewsItem {
     private(set) var thumbnail: String?
     private(set) var time: String?
     
+    private var formatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy.MM.dd"
+        return formatter
+    }()
+    private var nowDate: Date? {
+        let nowUTC = Date()
+        let timeZoneOffset = Double(TimeZone.current.secondsFromGMT(for: nowUTC))
+        return Calendar.current.date(byAdding: .second, value: Int(timeZoneOffset), to: nowUTC)
+    }
+    
     init(title: String, thumbnail: String, time: String) {
         self.title = title
         self.thumbnail = thumbnail
@@ -107,31 +143,20 @@ struct AlarmMyNewsItem {
         guard let thumbnail = thumbnail else { return nil }
         return URL(string: thumbnail)
     }
-    
-    // Date <-> String 하는 Common 함수가 있다면 그걸 사용
-    func getDateTime() -> Date? {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy.MM.dd"
-        guard let thumbnail = thumbnail else { return nil }
-        return formatter.date(from: thumbnail)
+    func getTimeDate() -> Date? {
+        guard let time = time else { return nil }
+        return formatter.date(from: time)
     }
-    
-    func getTimeString() -> String? {
-        if time == nil { return nil }
+    func getTimeString() -> String? { // TODO: 'ago' 공통정책 적용
+        guard let timeDate = getTimeDate() else { return nil }
+        guard let nowDate = nowDate else { return nil }
         
-        guard let timeDate = getDateTime() else { return nil }
+        let diff = Calendar.current.dateComponents([.day, .hour, .minute, .second], from: timeDate, to: nowDate) // diff = timeDate ~ nowDate
         
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy.MM.dd"
-        let nowDate = Date()
-        let nowStr = formatter.string(from: nowDate)
-        
-        // TODO: 'ago' 공통정책 적용
-        let diff = Calendar.current.dateComponents([.day, .hour, .second], from: timeDate, to: nowDate)
-        if nowStr == time {
-            return "\(diff.hour ?? 0)시간 전"
-        } else if (diff.day ?? 0 < 7) {
-            return "\(diff.day ?? 0)일 전"
+        guard let diffDay = diff.day else { return nil }
+        if diffDay == 0 { return "\(diff.hour ?? 0)시간 전" }
+        else if diffDay < 7 {
+            return "\(diffDay)일 전"
         } else {
             return time
         }
