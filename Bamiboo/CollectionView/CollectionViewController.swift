d66b85d2dbb737865618ef4e3cf62e590e89e835
Bamiboo <hobin1019@naver.com>
2020-12-15 14:04:39 +0900

    CollectionViewController shrink/expand Animation

--- Bamiboo/CollectionView/CollectionViewController.swift
+++ Bamiboo/CollectionView/CollectionViewController.swift
@@ -9,17 +9,19 @@ import UIKit
 
 private let reuseIdentifier = "Cell"
 
-class CollectionViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+class CollectionViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    var dataSource: [String] = ["first item\nhihi", "second item hello", "3rd ;laksjdf;laskdjf", "4!! hihihihihih\nllllasdkfjhslkdjhcxkmjvs\nhello"]
+    var isOpenedArr: [Bool] = [false, false, false, false]
     
     lazy var collectionView: UICollectionView = {
         let l = UICollectionViewFlowLayout()
         l.scrollDirection = .vertical
-        l.headerReferenceSize = CGSize(width: 100, height: 200)
-        l.estimatedItemSize = CGSize(width: view.bounds.width, height: 100)
+        l.headerReferenceSize = CGSize(width: 100, height: 150)
+        l.itemSize = CGSize(width: view.bounds.width, height: 100)
         
         let cv = UICollectionView(frame: .zero, collectionViewLayout: l)
         cv.translatesAutoresizingMaskIntoConstraints = false
-        cv.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        cv.register(CollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
         cv.delegate = self
         cv.dataSource = self
         return cv
@@ -29,9 +31,6 @@ class CollectionViewController: UIViewController, UICollectionViewDataSource, UI
         super.viewDidLoad()
 
         view.backgroundColor = .black
-    
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
         
         view.addSubview(collectionView)
         NSLayoutConstraint.activate([
@@ -49,42 +48,79 @@ class CollectionViewController: UIViewController, UICollectionViewDataSource, UI
         return 1
     }
 
-
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of items
-        return 100
+        return dataSource.count
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-        cell.backgroundColor = .red
-        
-        cell.contentView.translatesAutoresizingMaskIntoConstraints = false
-        cell.constraints.forEach { $0.isActive = false }
-        NSLayoutConstraint.activate([
-            cell.contentView.leadingAnchor.constraint(equalTo: cell.leadingAnchor),
-            cell.contentView.trailingAnchor.constraint(equalTo: cell.trailingAnchor),
-            cell.contentView.topAnchor.constraint(equalTo: cell.topAnchor),
-            cell.contentView.bottomAnchor.constraint(equalTo: cell.bottomAnchor),
-        ])
-
-        let label = UILabel()
-        label.numberOfLines = 0
-        label.text = ["hi\nhihi", "hello", "l;askjdf;laksjdf;laskdjf", "hihihihihih\nllllasdkfjhslkdjhcxkmjvs"][indexPath.row % 4]
-        cell.contentView.addSubview(label)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            label.leadingAnchor.constraint(equalTo: cell.contentView.leadingAnchor),
-            label.trailingAnchor.constraint(equalTo: cell.contentView.trailingAnchor),
-            label.topAnchor.constraint(equalTo: cell.contentView.topAnchor),
-            label.bottomAnchor.constraint(equalTo: cell.contentView.bottomAnchor),
-        ])
-        
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as? CollectionViewCell else { return UICollectionViewCell() }
+        cell.title = dataSource[indexPath.row]
         return cell
     }
 
     // MARK: UICollectionViewDelegate
-    func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        isOpenedArr[indexPath.row] = !isOpenedArr[indexPath.row]
+        
+        let open = isOpenedArr[indexPath.row]
+        if let cell = collectionView.cellForItem(at: indexPath) as? CollectionViewCell {
+            let maxSize = CGSize(width: collectionView.frame.width, height: CGFloat.infinity)
+            let expectedSize = NSString(string: self.dataSource[indexPath.row]).boundingRect(with: maxSize, options: .usesLineFragmentOrigin, attributes: CollectionViewCell.titleAttribute, context: nil)
+            let cellHeightTo: CGFloat = open ? 200 : expectedSize.height
+            
+            collectionView.performBatchUpdates({
+                collectionView.collectionViewLayout.invalidateLayout()
+            })
+            
+            UIView.animate(withDuration: 1, animations: {
+                cell.frame.size = CGSize(width: collectionView.frame.width, height: cellHeightTo)
+            })
+        }
     }
+    
+    
+    // MARK: UICollectionViewDelegateFlowLayout
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let maxSize = CGSize(width: collectionView.frame.width, height: CGFloat.infinity)
+        let expectedSize = NSString(string: dataSource[indexPath.row]).boundingRect(with: maxSize, options: .usesLineFragmentOrigin, attributes: CollectionViewCell.titleAttribute, context: nil)
+        let cellHeightTo: CGFloat = isOpenedArr[indexPath.row] ? 200 : expectedSize.height
+        
+        return CGSize(width: collectionView.frame.width, height: cellHeightTo)
+    }
+}
+
+class CollectionViewCell: UICollectionViewCell {
+    static let titleAttribute: [NSAttributedString.Key: Any] = [
+        .font: UIFont.boldSystemFont(ofSize: 20)
+    ]
+    var title: String {
+        get { titleLabel.text ?? ""}
+        set {
+            titleLabel.attributedText = NSAttributedString(string: newValue, attributes: CollectionViewCell.titleAttribute)
+            titleLabel.sizeToFit()
+        }
+    }
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.backgroundColor = .yellow
+        self.addSubview(label)
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        self.backgroundColor = .red
+        self.addSubview(titleLabel)
+    }
+    
 }
